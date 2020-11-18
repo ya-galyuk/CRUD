bd5befaa1de39680a9dc82f6cca944bb602d7c56
snowisecream <63078761+snowisecream@users.noreply.github.com>
2020-11-19 01:24:48 +0200

    del

--- Connect.js
+++
@@ -1,5 +0,0 @@
-const {development} = require('./config/config.json');
-const url = '' + development.dialect + '://' + development.username + ':' + development.password + '@' + development.host + ':' + development.port + '/' + development.database;
-const {Sequelize} = require('sequelize');//import
-const sequelize = new Sequelize(url);
-global.sequelize = sequelize;
